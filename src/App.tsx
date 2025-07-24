dcdda4d41a52f9ab0b1224f536205c8d31720217
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-24 17:04:47 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <SearchBar></SearchBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+function SearchBar(props) {
+  return (
+    <div className="bg-amber-700">
+      <input type="text" />
+      <button className="bg-amber-200">Bah</button>
+    </div>
+  );
+}
+
+export default SearchBar;
