9b3ef27938ac23e71c6de245bf5baa0724d9c409
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-30 23:33:26 -0400

    atualizações

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 function SearchBar(props) {
   return (
-    <div className="bg-amber-700">
-      <input type="text" />
-      <button className="bg-amber-200">Bah</button>
+    <div className="flex justify-center items-center">
+      <input
+        className=" border-2 border-gray-300 p-2 rounded-2xl hover:border-red-600 transition-all duration-300"
+        type="search"
+        placeholder="Buscar Filme"
+      />
     </div>
   );
 }
