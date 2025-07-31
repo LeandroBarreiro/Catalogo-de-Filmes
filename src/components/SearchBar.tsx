00749df74a392cd4f63c5819adf6d2c649067956
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-31 16:56:35 -0400

    SearchBar update

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
+const [search, setSearch] = useState("");
+
 function SearchBar(props) {
   return (
     <div className="flex justify-center items-center">
       <input
-        className=" border-2 border-gray-300 p-2 rounded-2xl hover:border-red-600 transition-all duration-300"
+        className=" border-2 border-gray-300 p-2 mt-20 rounded-2xl hover:border-red-600 transition-all duration-300"
         type="search"
         placeholder="Buscar Filme"
       />
